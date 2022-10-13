6d1b5b432afb9a318692cca5cb5227d168d5b812
philalexeev <philalexeev@yandex.ru>
2022-10-13 15:44:30 +0600

    add about section + fix hero dimentions

--- src/js/main.js
+++ src/js/main.js
@@ -17,3 +17,45 @@
 
     cloudsWrap.append(clone)
 })();
+
+// "About us" swiper initialization
+
+(function() {
+    const swiperScrollbar = document.querySelector(".swiper-scrollbar")
+    const breakpoint768 = window.matchMedia("(min-width: 768px)");
+    let swiper;
+
+    const enableSwiper = function() {
+        swiper = new Swiper('.swiper', {
+            // Optional parameters
+            slidesPerView: "auto",
+            spaceBetween: 15,
+            // And if we need scrollbar
+            scrollbar: {
+                el: '.swiper-scrollbar',
+                scrollbarDraggable: false,
+                scrollbarHide: false,
+                dragSize: "100"
+            },
+            on: {
+                setTranslate(sw, translate) {
+                    // swiperScrollbar.style.left = sw.passedParams.spaceBetween + translate * ((sw.virtualSize - swiperScrollbar.offsetWidth) / sw.virtualSize) + "px"
+                    swiperScrollbar.style.left = sw.passedParams.spaceBetween + swiperScrollbar.offsetWidth * (translate / sw.virtualSize) + "px"
+                }
+            }
+        });
+    }
+
+    const checkWindowWidth = function() {
+        if (breakpoint768.matches) {
+            if (swiper !== undefined) {
+                swiper.destroy(true, true)
+            }
+        } else {
+            enableSwiper()
+        }
+    }
+
+    checkWindowWidth()
+    window.onresize = checkWindowWidth
+})();
